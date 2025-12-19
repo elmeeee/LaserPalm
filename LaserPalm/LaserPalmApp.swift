e9f0cfc89a3f3042a3e29b2c95e2e341286d1d7b
elmeeee <37185272+elmeeee@users.noreply.github.com>
2025-12-19 16:36:56 +0700

    Initial Commit

---
+++ LaserPalm/LaserPalmApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaserPalmApp.swift
+//  LaserPalm
+//
+//  Created by Elmee on 19/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct LaserPalmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LaserPalmTests/LaserPalmTests.swift
@@ -0,0 +1,17 @@
+//
+//  LaserPalmTests.swift
+//  LaserPalmTests
+//
+//  Created by Elmee on 19/12/2025.
+//
+
+import Testing
+@testable import LaserPalm
+
+struct LaserPalmTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
